9629ce71222c7296dca8a932091d726d56cbd2e3
AtylaDaSilva <atylamendes97@gmail.com>
2022-10-06 15:47:16 -0300

    Create convertTime.php

---
+++ convertTime/convertTime.php
@@ -0,0 +1,39 @@
+<?php
+    //Converts time in a 00:00:00 format to seconds, minutes or hours as an integer value
+    function convertTime($time = "00:00:00", $param) {
+        if (!isset($time) &&
+            $time == "00:00:00" &&
+            !isset($param) &&
+            $param != "TO_SECONDS" &&
+            $param != "TO_MINUTES" &&
+            $param != "TO_HOURS"
+        ) return null;
+
+        $t = explode(":", $time);
+        $h = (int) $t[0];
+        $m = (int) $t[1];
+        $s = (int) $t[2];
+
+        if ($param == "TO_SECONDS") {
+            //Convert to seconds
+            $res = ($h * 3600) + ($m * 60) + $s;
+        }
+
+        if ($param == "TO_MINUTES") {
+            //Convert to minutes
+            $res = ($h * 60) + $m + (intval($s/60));
+            
+        }
+
+        if ($param == "TO_HOURS") {
+            //Convert to hours
+            $res = $h + ($m / 60) + ($s / 3600);
+        }
+
+        return $res;
+    }
+
+    $res = convertTime("00:00:12000", "TO_HOURS");
+
+    echo $res;
+?>
