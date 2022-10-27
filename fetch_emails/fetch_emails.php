38a94de7fe7d0ad6b6ea0f7075f0ad713c5f851f
AtylaDaSilva <atylamendes97@gmail.com>
2022-10-27 16:07:36 -0300

    Various updates to fetch_emails

--- fetch_emails/fetch_emails.php
+++ fetch_emails/fetch_emails.php
@@ -1,70 +1,38 @@
 <?php
-
 class Inbox
 {
-    private $server;
-    private $user;
-    private $password;
-    private $inbox;
-    private $emailIDs;
-    private $emails = array();
-
-    public function __construct($server, $user, $password)
+    //Opens IMAP connection. Returns an IMAP\Connection resource on success, or false on failure.
+    public function connect($server, $user, $password)
     {
-        $this->server = $server;
-        $this->user = $user;
-        $this->password = $password;
-        $this->connect();
-    }
+        $resource = imap_open($server, $user, $password) or die("Could not connect to email service: " . imap_last_error());
 
-    //Opens IMAP connection. Returns an IMAP\Connection instance on success, or false on failure.
-    public function connect()
-    {
-        if (!isset($this->inbox)) {
-
-            return $this->inbox = imap_open($this->server, $this->user, $this->password) or die("Could not connect to email service: " . imap_last_error());
-        }
-
-        return false;
+        return $resource;
     }
 
     //Closes IMAP connnection. Returns true on success, or false on failure.
-    public function disconnect()
+    public function disconnect($imap_conn)
     {
-        if (isset($this->inbox)) {
-            $inbox = $this->inbox;
-            $this->inbox = "";
 
-            return imap_close($inbox);
-        }
-
-        return false;
+        return imap_close($imap_conn);
     }
 
-    //Returns an array containing the UID of all unseen emails.
-    public function listNewEmails()
+    //Returns an array containing the UID of UNSEEN emails from inbox, or false if no emails in $inbox match the criteria.
+    public function listNewEmails($inbox)
     {
-        if (isset($this->inbox) && $this->inbox != false) {
-            return $this->emailIDs = imap_search($this->inbox, "UNSEEN", SE_UID);
-        }
-
-        return false;
+        return imap_search($inbox, "UNSEEN", SE_UID);
     }
 
-    public function fetchEmails()
+    //Returns an array of emails, or null if no emails were listed
+    public function fetchEmails($inbox, $emailIDs)
     {
-        if (
-            isset($this->inbox) && $this->inbox != false &&
-            isset($this->emailIDs) && $this->emailIDs != false
-        ) {
-
+        if ($inbox && count($emailIDs) > 0) {
             $i = 0;
-            foreach ($this->emailIDs as $email) {
-                $overview = imap_fetch_overview($this->inbox, $email, FT_UID);
-                $structure = imap_fetchstructure($this->inbox, $email, FT_UID);
-                $body = imap_fetchbody($this->inbox, $email, "1", FT_UID); //"1" => TEXT/PLAIN
+            foreach ($emailIDs as $email) {
+                $overview = imap_fetch_overview($inbox, $email, FT_UID);
+                $structure = imap_fetchstructure($inbox, $email, FT_UID);
+                $body = imap_fetchbody($inbox, $email, "1", FT_UID); //"1" => TEXT/PLAIN
 
-                $this->emails[$i] = array(
+                $emails[$i] = array(
                     "email_id" => $email,
                     "structure" => $structure,
                     "overview" => $overview,
@@ -76,7 +44,7 @@ class Inbox
                 /* if any attachments are found... */
                 if (isset($structure->parts) && count($structure->parts)) {
                     for ($j = 0; $j < count($structure->parts); $j++) {
-                        $this->emails[$i]["attachments"][$j] = array(
+                        $emails[$i]["attachments"][$j] = array(
                             'is_attachment' => false,
                             'filename' => '',
                             'name' => '',
@@ -86,8 +54,8 @@ class Inbox
                         if ($structure->parts[$j]->ifdparameters) {
                             foreach ($structure->parts[$j]->dparameters as $object) {
                                 if (strtolower($object->attribute) == 'filename') {
-                                    $this->emails[$i]["attachments"][$j]['is_attachment'] = true;
-                                    $this->emails[$i]["attachments"][$j]['filename'] = imap_utf8($object->value);
+                                    $emails[$i]["attachments"][$j]['is_attachment'] = true;
+                                    $emails[$i]["attachments"][$j]['filename'] = imap_utf8($object->value);
                                 }
                             }
                         }
@@ -95,38 +63,38 @@ class Inbox
                         if ($structure->parts[$j]->ifparameters) {
                             foreach ($structure->parts[$j]->parameters as $object) {
                                 if (strtolower($object->attribute) == 'name') {
-                                    $this->emails[$i]["attachments"][$j]['is_attachment'] = true;
-                                    $this->emails[$i]["attachments"][$j]['name'] = imap_utf8($object->value);
+                                    $emails[$i]["attachments"][$j]['is_attachment'] = true;
+                                    $emails[$i]["attachments"][$j]['name'] = imap_utf8($object->value);
                                 }
                             }
                         }
 
-                        if ($this->emails[$i]["attachments"][$j]['is_attachment']) {
-                            //The $section parameter of imap_fetchbody behaves differently if the email has any attachments, so it was necessary to fetch email body again with a different section argument
-                            $this->emails[$i]["body"] = imap_fetchbody($this->inbox, $email, "1.1", FT_UID); //"1.1" => TEXT/PLAIN
-                            $this->emails[$i]["attachments"][$j]['attachment'] = imap_fetchbody($this->inbox, $email, $j + 1, FT_UID);
+                        if ($emails[$i]["attachments"][$j]['is_attachment']) {
+                            //The $section parameter of imap_fetchbody behaves differently if the email has any attachments, so it was necessary to fetch email body again with a different $section argument
+                            $emails[$i]["body"] = imap_fetchbody($inbox, $email, "1.1", FT_UID); //"1.1" => TEXT/PLAIN
+                            $emails[$i]["attachments"][$j]['attachment'] = imap_fetchbody($inbox, $email, $j + 1, FT_UID);
                         }
                     }
                 }
 
                 //Decodes body text from Quoted-Printable format
-                $this->emails[$i]["body"] = quoted_printable_decode($this->emails[$i]["body"]);
+                $emails[$i]["body"] = quoted_printable_decode($emails[$i]["body"]);
 
                 $i++;
             }
 
-            return $this->emails;
+            return $emails;
         }
 
         return null;
     }
 
-    /* Iterates through each email and download attachments */
-    public function downloadAttachments()
+    /* Iterates through each email and saves the attachments in the directory "./attachments/dd-mm-yy/email_id". Returns the number of attachments downloaded, or false if no attachments were downloaded */
+    public function downloadAttachments($emails)
     {
-        if (isset($this->emails) && $this->emails) {
+        if ($emails && count($emails) > 0) {
             $attachmentsDownloaded = 0;
-            foreach ($this->emails as $email) { //Loops through each email
+            foreach ($emails as $email) { //Loops through each email
                 foreach ($email["attachments"] as $attachment) { //Loops through attachments
                     if ($attachment['is_attachment'] == 1) {
                         $filename = $attachment['name'];
@@ -135,13 +103,13 @@ class Inbox
 
                         if (empty($filename)) $filename = time() . ".dat";
 
-                        $folder = "attachments";
+                        $folder = "attachments/" . date("d-m-y") . "/" . $email["email_id"];
 
                         if (!is_dir($folder)) {
-                            mkdir($folder);
+                            mkdir($folder, 0777, true);
                         }
 
-                        $fp = fopen("./" . $folder . "/" . $email["email_id"] . "-" . $filename, "w+");
+                        $fp = fopen("./" . $folder . "/" . $filename, "w+");
                         fwrite($fp, base64_decode($attachment['attachment']));
                         fclose($fp);
 
@@ -150,7 +118,7 @@ class Inbox
                 }
             }
 
-            if ($attachmentsDownloaded > 0) return true;
+            return $attachmentsDownloaded;
         }
 
         return false;
